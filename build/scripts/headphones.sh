eebfe075f936f2e7a97d7a44d15e413115467f78
Rik Veenboer <rik.veenboer@gmail.com>
2015-12-30 12:08:57 +0100

    Cleanup make/build scripts, fix newlines in scripts

--- build/scripts/headphones.sh
+++ build/scripts/headphones.sh
@@ -1,1 +1,18 @@
-#!/bin/bashset -esource /build/configset -x## Environmentexport_env HEADPHONES_CONFIG /host/etc/headphones/config.iniexport_env HEADPHONES_CONFIG_DEFAULT /opt/headphones/config.iniexport_env HEADPHONES_DATA /host/var/lib/headphones## Headphonescd /optgit clone https://github.com/rembo10/headphones.git ## Encodersecho "deb http://ppa.launchpad.net/mc3man/trusty-media/ubuntu $(lsb_release -c -s) main" | sudo tee -a /etc/apt/sources.listapt-get updateapt_get_install_permanent --force-yes ffmpeg libmp3lame0 libavcodec-extra-54
+#!/bin/bash
+set -e
+source /build/config
+set -x
+
+## Environment
+export_env HEADPHONES_CONFIG /host/etc/headphones/config.ini
+export_env HEADPHONES_CONFIG_DEFAULT /opt/headphones/config.ini
+export_env HEADPHONES_DATA /host/var/lib/headphones
+
+## Headphones
+cd /opt
+git clone https://github.com/rembo10/headphones.git
+ 
+## Encoders
+echo "deb http://ppa.launchpad.net/mc3man/trusty-media/ubuntu $(lsb_release -c -s) main" | sudo tee -a /etc/apt/sources.list
+apt-get update
+apt_get_install_permanent --force-yes ffmpeg libmp3lame0 libavcodec-extra-54
