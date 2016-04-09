801d108d0e6857b54f0340d3702d21a96345c56a
Rik Veenboer <rik.veenboer@gmail.com>
2016-04-09 16:21:54 +0100

    Minor fixes to btsync container

--- build/boot/btsync.sh
+++ build/boot/btsync.sh
@@ -3,5 +3,5 @@ set -e
 if [[ ! -e $BTSYNC_CONFIG ]]; then
     mkdir -p `dirname $BTSYNC_CONFIG`
     /opt/btsync --dump-sample-config > $BTSYNC_CONFIG
-    sed -i "s,\(\"listen\"\s*:\).*,\1 \"0.0.0.0:8888\"\,," $BTSYNC_CONFIG
+    sed -i "s,// \(\"storage_path\"\).*,  \1 : \"$BTSYNC_DATA\"\,," $BTSYNC_CONFIG
 fi
