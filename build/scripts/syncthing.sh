2512152480af5042016ebc43e3812ded1f335e2f
Rik Veenboer <rik.veenboer@gmail.com>
2016-04-09 16:29:56 +0100

    Clean syncthing build script

--- build/scripts/syncthing.sh
+++ build/scripts/syncthing.sh
@@ -10,14 +10,6 @@ export_env SYNCTHING_VERSION 0.12.20
 ## Dependencies
 apt_install_temporary curl ca-certificates
 
-## Gosu
-# gpg --keyserver pgp.mit.edu --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
-# curl -o /usr/local/bin/gosu -SL "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture)"
-# curl -o /usr/local/bin/gosu.asc -L "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture).asc"
-# gpg --verify /usr/local/bin/gosu.asc
-# rm /usr/local/bin/gosu.asc
-# chmod +x /usr/local/bin/gosu
-
 # Syncthing
 cd /opt
 curl -L -o syncthing.tar.gz https://github.com/syncthing/syncthing/releases/download/v$SYNCTHING_VERSION/syncthing-linux-amd64-v$SYNCTHING_VERSION.tar.gz
@@ -27,4 +19,3 @@ mv syncthing-linux-amd64-v* syncthing
 rm -rf syncthing/etc
 rm -rf syncthing/*.pdf
 mkdir -p $SYNCTHING_CONFIG
-mkdir -p $SYNCTHING_DATA
