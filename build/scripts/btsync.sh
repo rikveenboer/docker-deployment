afa4ff5d6bf062b5cb4d786b30246c315a65f850
Rik Veenboer <rik.veenboer@gmail.com>
2016-04-20 22:24:55 +0100

    Minor corrections

--- build/scripts/btsync.sh
+++ build/scripts/btsync.sh
@@ -8,11 +8,12 @@ export_env BTSYNC_CONFIG /host/etc/btsync/btsync.conf
 export_env BTSYNC_DATA /host/var/btsync
 
 ## BTsync
-cd opt
+cd /opt
 wget -O btsync.tar.gz http://download-lb.utorrent.com/endpoint/btsync/os/linux-x64/track/stable
-tar xzf btsync.tar.gz
+tar xzf btsync.tar.gz btsync
 
 ## Remove installation files
 if [ $MODE == "minimal" ]; then
+    cd /opt
     rm btsync.tar.gz
 fi
