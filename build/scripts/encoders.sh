dbb9904da6c3ccc0572290d895efa3af8aa0c7da
Rik Veenboer <rik.veenboer@gmail.com>
2016-04-20 22:18:38 +0100

    Move encoders and ffmpeg to separate scripts

---
+++ build/scripts/encoders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+source /build/config
+set -x
+
+## Enable multimedia sources in APT
+echo 'deb http://www.deb-multimedia.org jessie main non-free' >> /etc/apt/sources.list
+echo 'deb-src http://www.deb-multimedia.org jessie main non-free' >> /etc/apt/sources.list
+apt update
+apt_install_permanent --force-yes deb-multimedia-keyring
+
+## Encoders
+apt_install_permanent --force-yes libmp3lame0 libavcodec-extra* libmp3lame-dev libvorbis-dev libtheora-dev libspeex-dev libopenjpeg-dev libx264-dev libfaac-dev
