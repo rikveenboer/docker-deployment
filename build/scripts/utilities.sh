6a747d3c4ae12032282387221cf904b28701c7e9
Rik Veenboer <rik.veenboer@gmail.com>
2016-04-20 22:22:51 +0100

    Add psmisc package to utilities for killall command

--- build/scripts/utilities.sh
+++ build/scripts/utilities.sh
@@ -4,7 +4,7 @@ source /build/config
 set -x
 
 ## Often used tools
-apt_install_permanent wget curl python-pip git gawk zip gperf unzip inetutils-ping inetutils-telnet
+apt_install_permanent psmisc wget curl python-pip git gawk zip gperf unzip bzip2 inetutils-ping inetutils-telnet rsync
 
 ## Often used python modules
 pip install argparse
