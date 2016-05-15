1f5f85cfcafa31018d3b6b5fa244f2a8075f0a3c
Rik Veenboer <rik.veenboer@gmail.com>
2016-05-15 22:29:30 +0100

    add files for csync container

---
+++ build/boot/csync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# if [ ! -e $CSYNC_CONFIG ]; then
+    # cp /usr/local/etc/csync2* /host/etc/
+# fi
+# rm -r /usr/local/etc
+# csync2 -k /host/etc/csync2.pem
+# ln -sf /host/etc/ /usr/local/etc
