44a78013304a8dc7e3d56ca310133aba683dc504
Rik Veenboer <rik.veenboer@gmail.com>
2016-05-16 08:27:22 +0100

    add sshd to csync

--- build/boot/csync.sh
+++ build/boot/csync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-# if [ ! -e $CSYNC_CONFIG ]; then
-    # cp /usr/local/etc/csync2* /host/etc/
-# fi
-# rm -r /usr/local/etc
-# csync2 -k /host/etc/csync2.pem
-# ln -sf /host/etc/ /usr/local/etc
+HOST_ETC_CSYNC=/host/etc/csync
+if [ ! -e $CSYNC_CONFIG ]; then
+    cp /usr/local/etc/csync2* $HOST_ETC_CSYNC/
+    csync2 -k $HOST_ETC_CSYNC/csync2.pem
+fi
+rm -r /usr/local/etc
+ln -sf $HOST_ETC_CSYNC/ /usr/local/etc
