0cec1c79c68e324422a96dada2b5e4ddffa943ef
Rik Veenboer <rik.veenboer@gmail.com>
2016-05-23 22:54:54 +0200

    add missing dependencies to couchpotato container

--- build/scripts/couchpotato.sh
+++ build/scripts/couchpotato.sh
@@ -2,6 +2,9 @@
 export_env COUCHPOTATO_CONFIG /host/etc/couchpotato/config.ini    
 export_env COUCHPOTATO_DATA /host/var/lib/couchpotato
 
+## Dependencies
+apt_install_permanent python-openssl python-lxml
+
 ## CouchPotato
 cd /opt
 git clone https://github.com/RuudBurger/CouchPotatoServer.git
