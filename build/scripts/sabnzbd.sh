2cc7047e7f827fe5ab37d1813eecb43608bbf061
Rik Veenboer <rik.veenboer@gmail.com>
2016-12-27 10:30:12 +0100

    update rar version and add 7zip in sabnzbd container

--- build/scripts/sabnzbd.sh
+++ build/scripts/sabnzbd.sh
@@ -1,9 +1,7 @@
 ## Environment
-export_env RAR_VERSION 4.1.1
+export_env RAR_VERSION 5.4.0
 export_env SABNZBD_CONFIG /host/etc/sabnzbd/sabnzbd.ini
 export_env SABNZBD_DATA /host/var/lib/sabnzbd
-
-
 
 ## SABnzbd
 echo 'deb http://ppa.launchpad.net/jcfp/ppa/ubuntu precise main' > /etc/apt/sources.list.d/sabnzbdplus.list
@@ -19,3 +17,6 @@ cd /opt
 wget http://www.rarlab.com/rar/rarlinux-x64-$RAR_VERSION.tar.gz
 tar xzf rarlinux-x64-$RAR_VERSION.tar.gz
 ln -s /opt/rar/rar /usr/bin
+
+## 7zip
+apt_install_permanent p7zip-full
