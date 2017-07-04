1cfadd569f9e4d4ead67a6fb8652d8380e613e0f
Rik Veenboer <rik.veenboer@gmail.com>
2017-07-04 10:45:17 +0200

    start fixing ffmpeg and kodi builds from debian stretch

--- build/scripts/ffmpeg.sh
+++ build/scripts/ffmpeg.sh
@@ -1,9 +1,21 @@
+## Configuration
+export_env FFMPEG_VERSION 3.3.2
+export_env SDK_VERSION 8.0.14
+export_env SDK_BASENAME Video_Codec_SDK_$SDK_VERSION
+export_env SDK_URL https://www.dropbox.com/s/wel0xazy2c45pqe/$SDK_BASENAME.zip
+
 ## Dependencies
-apt_install_permanent zip sudo
+apt_install_permanent sudo linux-libc-dev
 
 ## FFmpeg
 cd /opt
 git clone https://github.com/lutris/ffmpeg-nvenc.git
+sed -i "s,\(ffmpeg_version=\).*,\1\"$FFMPEG_VERSION\"," /opt/ffmpeg-nvenc/build.sh
+sed -i "s,\s*--enable-x11grab \\\s*,," /opt/ffmpeg-nvenc/build.sh
+sed -i "s,\s*--enable-shared \\\s*,," /opt/ffmpeg-nvenc/build.sh
+sed -i "s,\s*--extra-ldexeflags.*,," /opt/ffmpeg-nvenc/build.sh
+sed -i "s,\(sdk_url=\).*,\1\"$SDK_URL\"," /opt/ffmpeg-nvenc/build.sh
+sed -i "s,\(sdk_basename=\).*,\1\"$SDK_BASENAME\"," /opt/ffmpeg-nvenc/build.sh
 /opt/ffmpeg-nvenc/build.sh
 
 ## Enable libraries and binaries
