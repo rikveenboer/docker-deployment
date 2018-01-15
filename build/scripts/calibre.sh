b500b0c6053b1cfa3102d79faa3aaec17e8ff1aa
Rik Veenboer <rik.veenboer@gmail.com>
2018-01-15 17:58:03 +0000

    fix calibre container

--- build/scripts/calibre.sh
+++ build/scripts/calibre.sh
@@ -3,8 +3,10 @@ export_env CALIBRE_LIBRARY /host/calibre/library
 export_env CALIBRE_LOG /host/calibre.log
 
 ## Dependencies
-apt_install_permanent python-qt4 xz-utils xdg-utils imagemagick
+apt_install_permanent python-qt4 xz-utils xdg-utils imagemagick libglu1
 
 ## Calibre
 cd /opt
-wget --no-check-certificate-nv -O- http://download.calibre-ebook.com/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+wget https://download.calibre-ebook.com/linux-installer.py
+unset http_proxy
+python linux-installer.py
