d504ce54e29605a465dfd648eaab1f8826907699
Bram Veenboer <bram.veenboer@gmail.com>
2020-02-23 17:54:21 +0100

    Fixes to make btrfs work (without external btrfs binary)

--- build/scripts/collectd.sh
+++ build/scripts/collectd.sh
@@ -1,3 +1,7 @@
-apt_install_permanent libsensors5 hddtemp collectd
+apt_install_permanent libsensors5 liblzo2-2 hddtemp collectd sudo btrfs-progs
 
 pip install requests
+
+useradd collectd
+usermod -aG sudo collectd
+echo 'collectd ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/collectd
