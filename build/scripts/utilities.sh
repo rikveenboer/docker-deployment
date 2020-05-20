1e110e316bc169ecc5a54010f0e52cd32e52d2c8
Bram Veenboer <bram.veenboer@gmail.com>
2020-05-20 19:45:33 +0200

    Update utilities for debian buster
    * gnupg2 -> gpgv
    * bzip2 -> lbzip2

--- build/scripts/utilities.sh
+++ build/scripts/utilities.sh
@@ -1,5 +1,5 @@
 ## Often used tools
-apt_install_permanent software-properties-common gnupg2 dirmngr psmisc wget curl python-pip git gawk zip gperf unzip bzip2 inetutils-ping inetutils-telnet rsync
+apt_install_permanent software-properties-common gpgv dirmngr psmisc wget curl python-pip git gawk zip gperf unzip lbzip2 inetutils-ping inetutils-telnet rsync
 
 ## Often used python modules
 pip install argparse
